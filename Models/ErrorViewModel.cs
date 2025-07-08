45bfacc7158329b3c89d310efe27c6e0f1179821
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-07-07 21:42:27 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Htsoft.Uttt.Edi.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
