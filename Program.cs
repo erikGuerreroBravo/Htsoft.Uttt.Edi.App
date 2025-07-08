d756dd6412e6634e3d66519200f713c3fe6aa301
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-07-07 22:31:26 -0600

    fix: se modifica el archivo de configuracion NLog

--- Program.cs
+++ Program.cs
@@ -1,29 +1,47 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+using NLog;
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+var logger = LogManager.Setup()
+                       .LoadConfigurationFromFile("nlog.config")
+                       .GetCurrentClassLogger();
+
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    logger.Info("Start la aplicacion");
+    var builder = WebApplication.CreateBuilder(args);
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    var app = builder.Build();
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapStaticAssets();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+        .WithStaticAssets();
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
-app.Run();
+catch (Exception ex)
+{
+    logger.Error(ex, "Se detuvo la aplicacion durante la ejecucion.");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
