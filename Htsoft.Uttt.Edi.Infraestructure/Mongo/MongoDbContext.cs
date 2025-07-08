31d67f73db5f10beacb7cb93acedd8422fec2392
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-07-07 22:56:57 -0600

    feat: agregamos la clase que se encarga de la configuracion base de mongo

--- Htsoft.Uttt.Edi.Infraestructure/Mongo/MongoDbContext.cs
+++ Htsoft.Uttt.Edi.Infraestructure/Mongo/MongoDbContext.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
 
 namespace Htsoft.Uttt.Edi.Infraestructure.Mongo
 {
-    class MongoDbContext
+    public class MongoDbContext
     {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IOptions<MongoDbSettings>)
+        {
+            
+        }
     }
 }
---
+++ Htsoft.Uttt.Edi.Infraestructure/Mongo/MongoDbSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Htsoft.Uttt.Edi.Infraestructure.Mongo
+{
+    public class MongoDbSettings
+    {
+        /// <summary>
+        /// Cadena de conexion para MongoDB.
+        /// </summary>
+        public string ConnectionString { get; set; } = string.Empty;
+        /// <summary>
+        /// Nombre de la base de datos MongoDB.
+        /// </summary>
+        public string Database { get; set; } = string.Empty;
+    }
+}
