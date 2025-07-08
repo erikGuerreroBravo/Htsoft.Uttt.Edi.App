b73c582d3edf5a97a13aa03187a3d4c58352046e
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-07-08 02:08:31 -0600

    fix: modificamos estructura del proyecto apra adecuar interfaces e implementaciones en clases.

---
+++ Controllers/ExcelUploadController.cs
@@ -0,0 +1,31 @@
+﻿using Htsoft.Uttt.Edi.Aplication.Interfaces;
+using Htsoft.Uttt.Edi.Aplication.Interfaces.Services;
+using Htsoft.Uttt.Edi.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+
+namespace Htsoft.Uttt.Edi.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExcelUploadController : ControllerBase
+    {
+        private readonly IExcelUploadService _service;
+        private readonly IEdiRepository _repository;
+
+        public ExcelUploadController(IExcelUploadService service, IEdiRepository repository)
+        {
+            _service = service;
+            _repository = repository;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            var data = await _service.ReadExcelAsync(file);
+            await _repository.BulkInsertAsync(data.Select(dto => dto.ToEntity()));
+            return Ok(new { message = "Carga exitosa", total = data.Count });
+        }
+    }
+}
